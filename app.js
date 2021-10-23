a4ce65492d7134b0d76351f5c20f767fd3309f8d
Olumide Gbenjo <48032898+OlumideG@users.noreply.github.com>
2021-10-23 20:55:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+   origin:"*",
+}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
